7db78abad34c7d5b3d3317be5c738235808f6732
Krisha Sharma <krvsharm>
2022-01-09 21:58:09 +0000

    figure 1 done

--- asgn1/plot.sh
+++ asgn1/plot.sh
@@ -2,23 +2,27 @@
 
 rm -f /tmp/length.dat
 
-for n in {1..10}; do  
-	echo -n $n \ >> /tmp/length.dat 
+for n in {1..10000}; do  
+	echo -n $n \ >> /tmp/length.dat     
 	./collatz -n $n | wc -l >> /tmp/length.dat
 done 
 
+for n in {1..10000}; do
+	echo
+	./collatz -n $n | sort -n | tail -n 1 >> /tmp/maximum.dat
 
 
 # make clean && make sincos   # Rebuild the sincos executable.
 # ./sincos > /tmp/sin.dat     # Place the data points into a file.
 
 # This is the heredoc that is sent to gnuplot.
-gnuplot <<END 
-    set terminal pdf
-    set output "length.pdf"
-    set title "Sequence lengths"
-    set xlabel "length"
-    set ylabel "n"
-    set zeroaxis
-    plot "/tmp/length.dat" with points title ""
-END 
+
+gnuplot <<END
+   set terminal pdf 
+   set output "length.pdf"
+   set title "Collatz Sequence Lengths"
+   set xlabel "x"
+   set ylabel "y"
+   set zeroaxis
+   plot "/tmp/length.dat" with dots title ""
+END
