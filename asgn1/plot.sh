3d6af9e0b3cc051ee073fa265e79b048e6a2fd12
Krisha Sharma <krvsharm>
2022-01-09 22:55:21 +0000

    working on figure 2

--- asgn1/plot.sh
+++ asgn1/plot.sh
@@ -8,9 +8,9 @@ for n in {1..10000}; do
 done 
 
 for n in {1..10000}; do
-	echo
+	echo -n $n \ >> /tmp/maximum.dat 
 	./collatz -n $n | sort -n | tail -n 1 >> /tmp/maximum.dat
-
+done
 
 # make clean && make sincos   # Rebuild the sincos executable.
 # ./sincos > /tmp/sin.dat     # Place the data points into a file.
@@ -25,4 +25,11 @@ gnuplot <<END
    set ylabel "y"
    set zeroaxis
    plot "/tmp/length.dat" with dots title ""
+   set terminal pdf 
+   set output "maximum.pdf"
+   set title "Maximum Collatz Sequence Value"
+   set xlabel "n"
+   set ylabel "value"
+   set zeroaxis 
+   plot "/tmp/maximum.dat" with dots title ""
 END
