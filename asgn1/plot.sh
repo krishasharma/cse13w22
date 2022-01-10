98b17b4f04c22130bccaec095bfef2c409d1a32e
Krisha Sharma <krvsharm>
2022-01-10 01:23:01 +0000

    working on figure 3

--- asgn1/plot.sh
+++ asgn1/plot.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
+# collatz sequence lengths for loop
 rm -f /tmp/length.dat
-
 for n in {1..10000}; do  
 	echo -n $n \ >> /tmp/length.dat     
 	./collatz -n $n | wc -l >> /tmp/length.dat
 done 
 
-rm -f /tmp/maxium.dat
 
+# maximum collatz sequence value for loop 
+rm -f /tmp/maxium.dat
 for n in {1..10000}; do
 	echo -n $n \ >> /tmp/maximum.dat 
 	./collatz -n $n | sort -n | tail -n 1 >> /tmp/maximum.dat
 done
+
+
+# collatz sequence lengths histogram 
+rm -f /tmp/length.dat 
+for n in {1:10000}; do 
+	./collatz -n $n | wc -l >> /tmp/length.dat
+done 
+cat /tmp/length.dat | uniq -c | less >> /tmp/histogram.dat 
 
 # make clean && make sincos   # Rebuild the sincos executable.
 # ./sincos > /tmp/sin.dat     # Place the data points into a file.
 
 # This is the heredoc that is sent to gnuplot.
 
+
+# collatz sequence lengths 
 gnuplot <<END
    set terminal pdf 
    set output "length.pdf"
@@ -27,6 +38,11 @@ gnuplot <<END
    set ylabel "y"
    set zeroaxis
    plot "/tmp/length.dat" with dots title ""
+END
+
+
+# maximum collatz sequence value
+gnuplot <<END
    set terminal pdf 
    set output "maximum.pdf"
    set title "Maximum Collatz Sequence Value"
@@ -34,4 +50,14 @@ gnuplot <<END
    set ylabel "value"
    set yrange [0:100000] 
    plot "/tmp/maximum.dat" with dots title ""
+END
+
+# collatz sequence length histogram 
+gnuplot <<END
+   set terminal pdf 
+   set output "histogram.pdf"
+   set title "Collatz Sequence Length Histogram" 
+   set xlabel "length"
+   set ylabel "frequency" 
+   plot "/tmp/histogram.dat" using 2:1 with impulse title ""
 END
