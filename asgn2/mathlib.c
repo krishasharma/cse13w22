5d7e8cef5de90264d513ac3c29386c68c67b2319
Krisha Sharma <krvsharm>
2022-01-16 06:20:00 +0000

    mathlib.c added

--- asgn2/mathlib.c
+++ asgn2/mathlib.c
@@ -1,77 +1,67 @@
 #include <stdio.h>
-#include <mathlib.h>
+#include "mathlib.h"
 #include <stdlib.h>
 
-# main function 
-int main(void) {
-	printf("sin - %.151f\n", Sin(4));	
-	return 0;
+#define EPSILON 1e-14 
+
+//modulus function 
+double Modulus(double x) {
+        double n;
+        double m;
+        return n - m * (int) (n / m);
 }
 
-# sin function 
-double Sin(double x) {
-	double t = x;
-        double s = x; // 1st term 
-	for (double n = 3.0, p = -1, abs(t) > EPSILON, n += 2.0, p *= -1) {
-		t *= (x / n) * (x / (n - 1)); // next odd term
-		s += p * t; // add it in
-		test = 5: 
-		printf("test");
-	}	
-	return s; 
-}
 
-# cos function 
-double Cos(double x) {
-	double t = 1.0;
-	double s = 1.0; // 0th term
-	for (double n = 2.0, p = -1; abs(t) > EPSILON; n += 2.0, p *= -1) {
-		t *= (x / n) * (x / (n-1)); // next even term 
-		s += p * t; // add it in 
-	}
-	return s; 
-} 
-
-# square root function 
-double Sqrt(double x) {
-	double f = 1.0; 
-	while (double z = 0.0, double y = 1.0, x > 4) {
-		x /= 4.0; 
-		f *= 2.0; 
-	}
-	while (z = y, abs(y - z) > EPSILON) {
-		y = 0.5 * (z + x / z);
-	}
-	return y * f;
-}
-
-# log function 
-double Log(double x) {  
-	double y = 1.0;
-	double p = Exp(y); // first guess 
-	while (abs(p - x) > EPSILON) {
-		y = y + (x-p) / p;
-		p = Exp(y); 
-	}
-	return y; 
-}
-
-# exponent function 
+//exponent function
 double Exp(double x) {
-	double trm = 1.0 
-	double trm = sum 
-	double k = 1 
-	while (trm = 1.0, sum = trm, k = 1, trm > EPLISON) {
-		trm *= abs(x) / k; 
-		sum += trm; 
-		k += 1
-	if (x > 0) {
-		return sum 
-	}
-	else {
-		1 / sum;  
-	}
-	}
+        double trm = 1.0;
+        double trm = sum;
+        double k = 1;
+        while (trm > EPSILON) {
+                trm *= Abs(x) / k;
+                sum += trm;
+                k += 1;
+        }
+}
+
+
+//sin function
+double Sin(double x) {
+        double s = 1.0;
+        double v = x;
+        double t = x;
+        double k = 3.0;
+        while (Abs(t) > EPSILON) {
+                t = t * (x * x) / ((k - 1) * k);
+                s = -s;
+                v += s * t;
+                k += 2.0;
+        }
+        return v;
+}
+
+
+//square root function
+double Sqrt(double x) {
+        double z = 0.0;
+        double y = 1.0;
+        while (Abs(y - z) > EPSILON) {
+                z = y;
+                y = 0.5 * (z + x / z);
+        }
+        return y;
+}
+
+
+//log function
+double Log(double x) {
+        double y = 1.0;
+        double p = Exp(y); // first guess
+        while (Abs(p - x) > EPSILON) {
+                y = y + x / p - 1;
+                p = Exp(y);
+        }
+        return y;
 }
 
 
