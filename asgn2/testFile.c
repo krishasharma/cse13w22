fdc3c0cad0e71eb3d24b0f6cbe0180342743ac40
Krisha Sharma <krvsharm>
2022-01-16 08:35:23 +0000

    testfile updated

--- asgn2/testFile.c
+++ asgn2/testFile.c
@@ -1,3 +1,20 @@
-#include <mathlib.h> 
+#include "mathlib.h" 
+#include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+
+int main(void) {
+	//printf("TEST");
+	//printf("%f/n", Sin(0));
+	printf("Sin Difference:\n");
+	for (double i = 10; i < 30; i ++){
+		printf("DIFFERENCE = (%.15lf)\n", Sin(i) - sin(i));
+	}
+	printf("Cos Difference:\n");
+	for (double i = 10; i < 30; i ++){
+                printf("DIFFERENCE = (%.15lf)\n", Cos(i) - cos(i));
+        }		
+	return 0; 
+}
+
 
