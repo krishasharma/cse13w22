33f6556dba336282fb8833282511422ffc574c75
Krisha Sharma <krvsharm>
2022-01-27 06:24:47 +0000

    heap.c done

--- asgn3/heap.c
+++ asgn3/heap.c
@@ -4,30 +4,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-uint32_t max_child(uint32_t *A, uint32_t first, uint32_t last) {
+// this code is based off of pusedocode provided by Professor Long 
+// the pusedocode was given in the assignment PDF and the discord
+// please note that I have high psuedo code collaboration with my sister Twisha Sharma (tvsharma)
+
+// you can pass this through stats 
+uint32_t max_child(Stats *stats, uint32_t *A, uint32_t first, uint32_t last) {
     uint32_t left = 2 * first;
     uint32_t right = left + 1;
-    if (right <= last && A[right - 1] > A[left - 1]) {
+    if (right <= last && (cmp(stats, A[right - 1], A[left - 1])) > 0) { //(right <= last && A[right - 1] > A[left - 1]) {
 	return right;
     }
     else {
         return left;
     }
 }
-
-void fix_heap(uint32_t *A, int first, int last) {
+// you can pass this through stats 
+void fix_heap(Stats *stats, uint32_t *A, uint32_t first, uint32_t last) {
     bool found = false;
-    int mother = first;
-    int great = max_child(A, mother, last);
-    while (mother <= last / 2 && mother != found) {
-        if (A[mother -1] < A[great - 1]) {
-            int x = A[mother - 1]; // parallel assignment, swap the variables
-            int y = A[great - 1];
-            int tempvar = x;
-            x = y; 
-            y = tempvar; // end parallel assignment
-            mother = great;
-            great = max_child(A, mother, last);
+    uint32_t mother = first;
+    uint32_t great = max_child(stats, A, mother, last);
+    while (mother <= last / 2 && found == false) {
+        if (cmp(stats, A[mother -1], A[great - 1]) < 0) {
+	    swap(stats, &A[mother - 1], &A[great - 1]);
+	    mother = great;
+            great = max_child(stats, A, mother, last);
         }
         else {
            found = true;
@@ -35,20 +36,18 @@ void fix_heap(uint32_t *A, int first, int last) {
     }
 }
 
-void build_heap(uint32_t *A, int first, int last) {
-    for (int father = last / 2; father < first - 1; father -= 1) { // this loop might be wrong 
-        fix_heap(A, father, last);
+void build_heap(Stats *stats, uint32_t *A, uint32_t first, uint32_t last) {
+    for (uint32_t father = last / 2; father > first - 1; father -= 1) { // this loop might be wrong 
+        fix_heap(stats, A, father, last);
     }
 }
 
 void heap_sort(Stats *stats, uint32_t *A, uint32_t n) {
-    int first = 1;
-    int last = n;
-    build_heap(A, first, last);
-    for (int leaf = 1; leaf < last; leaf -= 1) {
-        // int x = A[first - 1]; 
-        // int y = A[leaf - 1];
+    uint32_t first = 1;
+    uint32_t last = n;
+    build_heap(stats, A, first, last);
+    for (uint32_t leaf = last; leaf > first; leaf -= 1) {
         swap(stats, &A[first - 1], &A[leaf - 1]); // parallel assignment
-        fix_heap(A, first, leaf - 1);
+        fix_heap(stats, A, first, leaf - 1);
     }
 }
