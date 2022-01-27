cc732c11b51aea5eeffc3c8526cd3e587d0aade1
Krisha Sharma <krvsharm>
2022-01-27 06:25:01 +0000

    quick.c done

--- asgn3/quick.c
+++ asgn3/quick.c
@@ -4,34 +4,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void partition(uint32_t *A, int lo, int hi) {
-    int i = lo - 1;
-    for (j = 1; j >= lo; j <= hi) {
-        if (A[j - 1] < A[hi - 1]) {
+// this code is based off of pusedocode provided by Professor Long 
+// the pusedocode was given in the assignment PDF and the discord
+// please note that I have high psuedo code collaboration with my sister Twisha Sharma (tvsharma) 
+
+uint32_t partition(Stats *stats, uint32_t *A, uint32_t low, uint32_t high) {
+    uint32_t i = low - 1;
+    for (uint32_t j = low; j < high; j += 1) { 
+    if (cmp(stats, A[j - 1], A[high - 1]) == -1) {
             i += 1;
-            int x = A[i - 1]; // parallel assignment 
-            int y = A[j - 1];
-            int temp = x;
-            x = y;
-            y = temp;
+	    swap(stats, &A[i - 1], &A[j - 1]);
         }
-    int d = A[i]; // parallel assignment
-    int h = A[hi - 1];
-    int t = d;
-    d = h;
-    h = t;
+	//swap(stats, &A[i], &A[high - 1]);
+    }
+    swap(stats, &A[i], &A[high - 1]);
     return i + 1;
+}
+
+void quick_sorter(Stats *stats, uint32_t *A, uint32_t low, uint32_t high) {
+    if (low < high) {
+        uint32_t p = partition(stats, A, low, high);
+        quick_sorter(stats, A, low, p - 1);
+        quick_sorter(stats, A, p + 1, high);
     }
 }
 
-void quick_sorter(uint32_t *A, int lo, int hi) {
-    if (lo < hi) {
-        int p = partition(A, lo, hi);
-        quick_sorter(A, lo, p - 1);
-        quick_sorter(A, p + 1, hi);
-    }
-}
-
-void quick_sort(uint32_t *A, uint32_t n) {
-    quick_sorter(A, 1, n);
+void quick_sort(Stats *stats, uint32_t *A, uint32_t n) {
+    quick_sorter(stats, A, 1, n);
 }
