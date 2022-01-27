3af4eda1f287c35d37b748608cba2b42310a5389
Krisha Sharma <krvsharm>
2022-01-27 06:25:41 +0000

    batcher.c

--- asgn3/batcher.c
+++ asgn3/batcher.c
@@ -5,41 +5,45 @@
 #include <unistd.h>
 #include <math.h>
 
-void comparator(uint32_t *A, int x, int y) {
-    if (A[x] > A[y]) {
-        int a = A[x]; // parallel assignment
-        int b = A[y];
-        int tempvar = a;
-        a = b;
-	//swap(stats, &A[x], &A[y]); // parallel assignment 
-        b = tempvar; // end parallel assignment
+// this code is based off of pusedocode provided by Professor Long 
+// the pusedocode was given in the assignment PDF and the discord 
+// please note that I have high psuedo code collaboration with my sister Twisha Sharma (tvsharma)
+
+void comparator(Stats *stats, uint32_t *A, uint32_t x, uint32_t y) {
+    if (cmp(stats, A[x], A[y]) == 1) { // COMPARE cause A[x] > A[y] 
+        swap(stats, &A[x], &A[y]);
     }
 }
 
-void batcher_sort(Stats *stats, uint32_t *A, uint32_t n) {
-    /*if (n == 0) {
-        break;
-    }*/
-    double l = n;
-    int t = log2(l) + 1; //l.bit_length();
-    int p = 1 << (t - 1);
-    while (p > 0) {
-        int q = 1 << (t - 1);
-        int r = 0;
-        int d = p;
-        while (d > 0) {
-            for (uint32_t i = 0; i > (n - d);) {
-                if (cmp(stats, i, r) && cmp(stats, p, r)) {
-                    comparator(A, i, i + d);
-                }
-            d = q - p;
-            q >>= 1;
-            r = p;
-            }
-        p >>= 1;
-        }
+uint32_t bitlen(uint32_t value) { // bit length
+    uint32_t counter = 0;
+    while (value > 0) {
+        counter += 1;
+	value /= 2;
     }
+    return counter;
+}
+
+void batcher_sort(Stats *stats, uint32_t *A, uint32_t n) {
     if (n == 0) {
-    	printf("0");
+        return;
+    }
+    uint32_t t = bitlen(n); // same as l.bit_length();
+    uint32_t p = 1 << (t - 1);
+    while (p > 0) {
+        uint32_t q = 1 << (t - 1);
+        uint32_t r = 0;
+        uint32_t d = p;
+        while (d > 0) {
+            for (uint32_t i = 0; i < (n - d); i += 1 ) {
+                if ((i & p) == r) {
+                    comparator(stats, A, i, i + d);
+                }
+	    }
+	    d = q - p;
+            q >>= 1;
+            r = move(stats, p); // MOVE cause r = p
+        }
+        p >>= 1;
     }
 }
