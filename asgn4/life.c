ab094936f803f7ed1abe9340a5cbe0ce2d092a8c
Krisha Sharma <krvsharm>
2022-02-03 23:49:13 +0000

    life.c done, asgn4 done

--- asgn4/life.c
+++ asgn4/life.c
@@ -8,11 +8,15 @@
 
 #define DELAY 50000
 
+// Please Note: The implemented code is based off of pusedocode provided by the Professor in the assignment PDF
+// There is also high level pusedocode collabration with my sister Twisha Sharma (tvsharm) 
+// The generations for loop is based off of pusedocode provided by Audrey Ostrom 
+// The ncurses implementation is based off of pusedocode given in the assignment PDF
+
 int main(int argc, char **argv) {
     uint32_t rows;
     uint32_t cols;
     bool toro = false;
-    //FILE *fill = fopen("life.txt", "r"); // infile was fill
     uint32_t generation = 0;
     int opt = 0;
 
@@ -25,7 +29,7 @@ int main(int argc, char **argv) {
     FILE *in = fopen("stdin", "r");
     FILE *out = fopen("stdout", "r");
     
-    printf("test\n");
+    // this getopt loop is based off of the example get opt loop Eugene showed during his section for asgn_2 
     while ((opt = getopt(argc, argv, "tsn:i:o:")) != -1) {
         switch (opt) {
         // -t specify that the Game of Life is to be played on a toroidal universe.
@@ -54,133 +58,59 @@ int main(int argc, char **argv) {
             break;
         }
     }
-    printf("test 1\n");
+
     fscanf(in, "%d %d", &rows, &cols);
-    printf("test A\n");
     Universe *uniA = uv_create(rows, cols, toro);
     Universe *uniB = uv_create(rows, cols, toro);
-    Universe *uniC = uv_create(rows, cols, toro);
-    printf("test B\n");
     uv_populate(uniA, in);
-    printf("test C\n");
     fclose(in);
 
-    
-   
-
-    printf("test D\n");
-    if (l_s) {
+    if (!l_s) {
         initscr();
         curs_set(FALSE);
     }
-	
-        for (uint32_t i = 0; i <= generation; i++) {
-            clear();
-            printf("test 3\n");
-            for (rows = 0; rows < uv_rows(uniA); rows++) {
-                printf("test 4\n");
-                for (cols = 0; cols < uv_cols(uniA); cols++) {
-                    printf("test 5\n");
-                    uint32_t censusval = uv_census(uniA, rows, cols);
-                    if ((uv_get_cell(uniA, rows, cols) == true) && ((censusval == 2) || (censusval == 3))) {
-                        printf("test 6\n");
-                        uv_live_cell(uniB, rows, cols);
-                        if(!l_s) {
-			    mvprintw(rows, cols, "o");
-			}
-                    }
-		    else if ((uv_get_cell(uniA, rows, cols) == false) && (censusval == 3)) {
-                        printf("test 7\n");
-                        uv_dead_cell(uniB, rows, cols);
-                        if (!l_s) {
-			     mvprintw(rows, cols, "o");
-			}
-                    } 
-		    else {
-                        printf("test 8\n");
-                        uv_dead_cell(uniB, rows, cols);
-                        if (!l_s){
-			    mvprintw(rows, cols, ".");
-			}
-                    }
-                    printf("test 9\n");
-                    uniC = uniA;
-                    uniA = uniB;
-                    uniB = uniC;
-                }
-            }
-            printf("test 10\n");
-            refresh();
-            usleep(DELAY);
-        }
-        printf("test 11\n");
-        endwin();
-        uv_print(uniA, out);
-        uv_delete(uniA);
-        uv_delete(uniB);
-        uv_delete(uniC);
-        fclose(out);   
-        return 0;    
-}
-
-    /*
-    initscr();
-    curs_set(FALSE);
     
-    printf("test 2\n");
-
-    for (uint32_t i = 0; i <= generation; i++) {
-        clear();
-	printf("test 3\n");
-        for (rows = 0; rows < uv_rows(uniA); rows++) {
-            printf("test 4\n");
+    // this code is based off of pusedocode from Audrey Ostrom's tutoring session on 02_02_2022
+    // there was also high level pusedocode collabration with my sister Twisha Sharma (tvsharma) 
+    // when attempting to begin coding ncurses and the generations for loop
+    for (uint32_t i = 0; i <= generation; i++) { 
+ 	clear();
+        for (rows = 0; rows < uv_rows(uniA); rows++) { 
             for (cols = 0; cols < uv_cols(uniA); cols++) {
-                printf("test 5\n");
                 uint32_t censusval = uv_census(uniA, rows, cols);
                 if ((uv_get_cell(uniA, rows, cols) == true) && ((censusval == 2) || (censusval == 3))) {
-                    printf("test 6\n");
                     uv_live_cell(uniB, rows, cols);
-                    mvprintw(rows, cols, "o");
-                } else if ((uv_get_cell(uniA, rows, cols) == false) && (censusval == 3)) {
-                    printf("test 7\n");
-		    uv_dead_cell(uniB, rows, cols);
-                    mvprintw(rows, cols, "o");
-                } else {
-		    printf("test 8\n");
-                    uv_dead_cell(uniB, rows, cols);
-                    mvprintw(rows, cols, ".");
+                    if(!l_s) {
+			mvprintw(rows, cols, "o");
+		    }
                 }
-		printf("test 9\n");
-                uniC = uniA;
-                uniA = uniB;
-                uniB = uniC;
+		else if ((uv_get_cell(uniA, rows, cols) == false) && (censusval == 3)) {
+                    uv_dead_cell(uniB, rows, cols);
+                    if (!l_s) {
+			mvprintw(rows, cols, "o");
+		    }
+                } 
+		else {
+                    uv_dead_cell(uniB, rows, cols);
+                    if (!l_s){
+			mvprintw(rows, cols, ".");
+	            }
+                }
+                Universe * temp = uniA;
+		uniA = uniB;
+                uniB = temp;
             }
+	    refresh();
+            usleep(DELAY);
         }
-	printf("test 10\n");
-        refresh();
-        usleep(DELAY);
     }
-    printf("test 11\n");
     endwin();
     uv_print(uniA, out);
     uv_delete(uniA);
     uv_delete(uniB);
-    uv_delete(uniC);
-    fclose(out);*/
 
-    // generations for loop pusedocode Audrey Ostrom tutoring session
-    // three for loops both universes are the same size, A is populated, B starts empty
-    // for (int i = 0; ___ ; i++) {
-    //    for (rows) {
-    //        for (cols) {
-    //            take a census; feed through uv_census
-    //            check the results and see how many neighbors its has
-    //            mvprintw(ROW, col, "o"); // where this goes depends on whether the cell is marked as live or dead
-    //            make the cells in univese B dead or alive based on the neighbors uv_live uv_get uv_dead
-    //        }
-    //    }
-    //    refresh();
-    //    usleep(DELAY);
-    // }
+    fclose(out);   
+    return 0;    
 
+}
 
