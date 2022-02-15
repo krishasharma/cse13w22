7fe5052a5165c478b3b4b581c6f4f971d5aeb975
Krisha Sharma <krvsharm>
2022-02-15 06:37:38 +0000

    randstate.c added

--- asgn5/randstate.c
+++ asgn5/randstate.c
@@ -11,14 +11,20 @@
 // decrypt.c 
 
 // Please Note:
-// The below code is based off of pseudocode provided during Audrey's tutoring section on ______
-
-/*struct state {
-    gmp_randstate_t state;
-    uint64_t seed;
-};*/
+// The implemented code is based off of pseudocode provided by the Professor in the assignment PDF
+// There is also high level pseudocode collabration with my sister Twisha Sharma (tvsharma)
+// Audrey is cited in randstate.c as she provided pusedocode for it during her tutoring section 
+// on 02_09_2022
+// Eugene is cited in numtheory.c for the pusedocode for the pow_mod and the is_prime functions 
+// given during his lab section on 02_04_2022
+// Eugene is also cited in numtheory.c make_prime specifically for for the idea of 
+// adding 2^bits + the random num generated from urandomb that caps at user input bits given
+// Omar is cited in numtheory.c for explaining how to implement the mpz functions from the GMP library
+// Brian is cited in rsa.c specifically in rsa_make_pub for explaining how to generate a random number
+// in the range specificed in the asgn_5 PDF 
 
 gmp_randstate_t state;
+
 
 void randstate_init(uint64_t seed) {
     // initializes a global random state named state with a Mersenne Twister algorithm 
@@ -26,12 +32,11 @@ void randstate_init(uint64_t seed) {
     // call srandom() using this seed as well 
     // call gmp_randinit_mt() and gmp_randseed_ui()
     // should be two or three lines MAX 
-    //srandom(seed);
     gmp_randinit_mt(state);
     gmp_randseed_ui(state, seed);
     srandom(seed);
-    //gmp_randinit_mt(state);
 }
+
 
 void randstate_clear(void) {
     // calls and frees all memory used by the inatilized global random state, state
